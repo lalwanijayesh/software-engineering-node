6b83e2228cd3c0e9f9aa3f16473bd6ea2c6c2c37
Jayesh Lalwani <lalwanijayesh09@gmail.com>
2022-11-06 15:47:56 -0500

    Add cors for server

--- server.ts
+++ server.ts
@@ -14,6 +14,7 @@
  */
 import express, {Request, Response} from 'express';
 import mongoose from "mongoose";
+import cors from 'cors';
 
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
@@ -24,6 +25,7 @@ import MessageController from "./controllers/MessageController";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 require('dotenv').config();
 
 const options = {
